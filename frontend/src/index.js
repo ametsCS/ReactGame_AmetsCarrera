3a4547c10866a2eca370de51a4a36aa007ecedda
ametsCS <carrera.amets@gmail.com>
2024-05-10 19:38:16 +0200

    hasiera

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,67 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import BoardView from "./components/Board";
 import "./main.scss";
 import "./styles.scss";
+//import { Consulta } from './components/Consulta';
 
-const App = () => {
+const Groq = require("groq-sdk");
+
+function App() {
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const groq = new Groq({
+          apiKey: process.env.REACT_APP_GROQ_API_KEY,
+          dangerouslyAllowBrowser: true
+        });
+        const chatCompletion = await groq.chat.completions.create({
+          messages: [
+            {
+              role: "user",
+              content: "Explain the importance of fast language models"
+            }
+          ],
+          model: "Llama3-70b-8192"
+        });
+        console.log(chatCompletion.choices[0]?.message?.content || "");
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+    fetchData();
+  }, []); // Empty dependency array ensures that this effect runs only once, similar to componentDidMount
+
+  /**   
+  useEffect(() => {
+  async function fetchData(content) {
+    try {
+      const groq = new Groq({
+        apiKey: process.env.REACT_APP_GROQ_API_KEY,
+        dangerouslyAllowBrowser: true
+      });
+      const chatCompletion = await groq.chat.completions.create({
+        messages: [
+          {
+            role: "user",
+            content: content // Pasamos el contenido como atributo
+          }
+        ],
+        model: "Llama3-70b-8192"
+      });
+      console.log(chatCompletion.choices[0]?.message?.content || "");
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+  
+  // Llamamos a fetchData con el contenido del mensaje deseado
+  fetchData("Explain the importance of fast language models");
+}, []); // Empty dependency array ensures that this effect runs only once, similar to componentDidMount
+
+  */
+
   return <BoardView />;
-};
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
